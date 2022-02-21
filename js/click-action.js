8b9492cb0f1324cd6f123f061fbeecb01f1514aa
Quy Nguyen <bk.nguyenvanquy@gmail.com>
2022-02-21 16:49:07 +0700

    update btn signup and download

--- js/click-action.js
+++ js/click-action.js
@@ -2,6 +2,17 @@ $(document).ready(function () {
   $("#hero-image-block").click(function () {
     $("#video-trailer").css({ display: "flex" });
   });
+  $("#download-btn").click(function () {
+    $("#down-load-modal-wrapper").css({ display: "flex" });
+  });
+  $("#download-btn-mobile").click(function () {
+    $("#down-load-modal-wrapper").css({ display: "flex" });
+  });
+
+
+  $("#down-load-modal-overlay").click(function () {
+    $("#down-load-modal-wrapper").css({ display: "none" });
+  });
 
   $("#hide-trailer").click(function () {
     $('iframe').attr('src', $('iframe').attr('src'));
--- js/scroll.js
+++ js/scroll.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top,
+          scrollTop: $(hash).offset()?.top,
         },
         800,
         function () {
